825222eefe39b9379bd841be2447669f1023260b
ldsvrn <louis.dsvrn@orange.fr>
2022-04-04 15:32:51 +0200

    update done

--- TP2/crud/urls.py
+++ TP2/crud/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ajout', views.ajout),
-    path('traitement', views.traitement),
-    path('show', views.show)
+    path('ajout/', views.ajout),
+    path('traitement/', views.traitement),
+    path('show/', views.show),
+    path('read/<int:id>/', views.read),
+    path('update/<int:id>/',views.update),
+    path('updatedb/<int:id>/',views.updatedb)
 ]
--- TP2/crud/views.py
+++ TP2/crud/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import LivreForm
 from . import models
+from django.http import HttpResponseRedirect
+from django.forms.models import model_to_dict
 
 
 def ajout(request):
@@ -27,3 +29,32 @@ def traitement(request):
 
 def show(request):
     return render(request,"show.html",{"Livre" : models.Livre.objects.all() })
+
+def read(request, id):
+    return render(request,"read.html",{"livre" : models.Livre.objects.get(pk=id)})
+
+
+def update(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    lform = LivreForm(model_to_dict(livre))
+    print(lform)
+    if request.method == "POST": 
+        form = LivreForm(request)
+        if form.is_valid(): # validation du formulaire.
+            livre = form.save() # sauvegarde dans la base
+            return render(request,"affiche.html",{"livre" : livre}) # envoie vers une page d'affichage du livre créé
+        else:
+            return render(request,"ajout.html",{"form": form})
+    else :
+        return render(request, "update.html", {"form": lform,"id":id})
+
+
+def updatedb(request, id):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        livre = lform.save(commit=False)
+        livre.id = id;
+        livre.save()
+        return HttpResponseRedirect("/crud/show/")
+    else:
+        return render(request, "update.html", {"form": lform, "id": id})
